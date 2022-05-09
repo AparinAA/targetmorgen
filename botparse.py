58dcfa81f248f5d754818d9856870fef32e32571
AparinAA <alxaparin@gmail.com>
2022-05-09 17:31:45 +0300

    + parse ID_CHANNEL +private

--- botparse.py
+++ botparse.py
@@ -15,15 +15,18 @@ load_dotenv()
 NAME_CHANNELS = (
     "alisherhateu", #Morgen
     "nemorgenshtern", #NEMORGEN news
-    'Full_Time_Trading', #FULL trading news
     'litvintm', #Litvin
     'toncoin_rus', #toncoin_news
-    'bogachnaprokach', #Lana
     'wallet_news', #Wallet news
     'tonwhalesnews', #WHALES news
     'durov', #Durov
     'slavamarlow', #Slava Marlow
-    'alxaparin', #test account
+    'givemetonru', #Дайте тон ньюс
+    #'alxaparin', #test account
+)
+ID_CHANNELS = (
+    258772623,
+    -1001535438409
 )
 
 #В этом словаре будут храниться связь ID : nameChannel
@@ -55,6 +58,25 @@ async def normal_handler(event):
     print(name_id_dict[event.message.chat.id])
     print("-"*103)
 
+#обработчки сообщений в канале ID_CHANNELS
+@client.on(events.NewMessage(chats=ID_CHANNELS))
+async def normal_handler(event):
+    print(event.message.date, end="\t |\t")
+    try:
+        #Если поймал кнопку с ссылкой
+        url = event.message.reply_markup.rows[0].buttons[0].url
+        await click(url)
+    except:
+        #Если поймал просто текст, то ищем в ней ссылку на wallet
+        check_url = re.findall(r'https?://t.me/wallet\?\S+',event.message.text)
+        if len(check_url) != 0:
+            url = check_url[0]
+            await click(url)
+        else:
+            print("NOT FOUND URL", end='\t|\t\t')
+    print(event.message.chat.id)
+    print("-"*103)
+
 def main():
     
     #стартуем и проверяем обновления
@@ -67,6 +89,11 @@ def main():
             print('NAME: {}, ID: {}'.format(name,msg.chat.id))
             name_id_dict[msg.chat.id] = name
     
+    name_id_dict['-1001535438409'] = 'KRIPTANAS'
+
+    for msg in client.get_messages(-1001535438409, limit=1):
+        print(msg)
+
     print('')
     print("="*103)
     print('DATE NEW POST\t\t\t |\tRESULT\t\t|\t\tNAME CHANNEL')
